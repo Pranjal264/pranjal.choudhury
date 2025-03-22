a3b382f527fab2d4877a890bbd4ba374d697075d
Pranjal Choudhury <pranjal.choudhury264@gmail.com>
2025-03-22 11:59:59 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navUl = document.querySelector("nav ul");
-
-  menuToggle.addEventListener("click", function() {
-      navUl.classList.toggle("show");
-  });
-});
-
-
-
 
 
 function openProfileModal() {
